cc7d6dd1551f653719204029fe17a4e5fb1e81f8
ary13 <ary13@duke.edu>
2019-11-22 23:26:43 -0500

    Update bash script. Not yet working, retire for evening

--- get_data.sh
+++ get_data.sh
@@ -1,40 +1,46 @@
 #!/bin/bash 
 
-sudo apt-get install unzip
-
 prepare_data() {
-    F = "soccer.zip"
-    m = "esdb.md5"
-    cd $1
-    if [[ -f "$F" ]]; then
-        echo "$F exists"
-    else
-        curl  https://www.kaggle.com/hugomathien/soccer/download > soccer.zip
-    cd $2
-    if [[ -f "$m"]]; then
-        echo "$m exists"
-    else
-        curl https://gitlab.oit.duke.edu/bios821/european_soccer_database/raw/master/esdb.md5 > esdb.md5
-    md5 $F > downloadm #save md5 character/number string downloaded(32,end)
-    cat downloadm > downloadS
-    cut -d ' ' -f 4 downloadS > substringD
-    cat $m > string #save md5 character/number string given(32,beginning)
-    cut -d ' ' -f 1 string > substring #cut to the first element of the string
-    #check congruence
-    if [$substringD == $substring]; then
-        cd $1
-        unzip $F -d $w3
+  echo "soccer.zip" > F
+  echo "esdb.md5" > m
+  cd $1
+  if [[ -f $F ]]; then
+    echo "$F exists"
+  else
+    curl  https://www.kaggle.com/hugomathien/soccer/download > soccer.zip
+    fi
+  cd $2
+  if [[ -f $m ]]; then
+    echo "$m exists"
+  else
+    curl https://gitlab.oit.duke.edu/bios821/european_soccer_database/raw/master/esdb.md5 > esdb.md5
+    fi
+  md5 esdb.md5 > downloadm #save md5 character/number string downloaded(32,end)
+  cat downloadm > downloadS
+  cut -d ' ' -f 4 downloadS > substringD
+  md5 $F > string #save md5 character/number string given(32,beginning)
+  cut -d ' ' -f 1 string > substring #cut to the first element of the string
+  #check congruence
+  if [$substringD == $substring]; then
+      cd $1
+      unzip $F -d $3
 	    echo "File has been downloaded and uncompressed."
-    else 
-	    echo "md5 do not match. Downloading database."
-        curl  https://www.kaggle.com/hugomathien/soccer/download > $1/soccer.zip
-        cd $download_dir
-        unzip $F -d $3
-        echo "File has been downloaded and uncompressed."
-	   
+	else
+	  echo "md5 do not match. Downloading database."
+	  curl  https://www.kaggle.com/hugomathien/soccer/download > $1/soccer.zip
+	  cd $3
+	  unzip $F -d $3
+	  echo "File has been downloaded and uncompressed."
+	  exit
     fi
     }
 
-exit
+prepare_data $HOME/Downloads  $HOME/Documents/GitLab/md5s  $HOME/Documents/GitLab
+
+
+
+
+
+
  
  
