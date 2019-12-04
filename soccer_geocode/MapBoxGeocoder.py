cb8485ec35f84d0cbf2cb39871303bce8c7f5628
ary13 <ary13@duke.edu>
2019-12-03 23:24:41 -0500

    Commiting frequently now because getting tired. Here, I am almost finished with the MapBoxGeocoder class

--- soccer_geocode/MapBoxGeocoder.py
+++ soccer_geocode/MapBoxGeocoder.py
@@ -18,21 +18,31 @@ class MapBoxGeocoder:
     '''A class of geocoding tools for the Mapbox API'''
 
     def __init__(self, MAP_PASS, countries):
-        self.map_pass = os.environ.get('MAP_PASS')
+        self.mappass = os.environ.get('MAP_PASS')
         self.url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
         self.countries = countries
+        self.ids = []
+        self.words = []
+        self.lat = []
+        self.long = []
         print("MapBoxGeocoder initiated")
 
+    @property
+    def label_countries(self):
+        for a,b in self.countries:
+            self.words.append(b)
+            self.ids.append(a)
 
-        @property
-        def charge(self):
-            return self._charge
-## Check connection
-def sql_connection():
-    try:
-        con = sqlite3.connect(':memory:')
-        print("Connection is established: Database is created in memory")
-    except Error:
-        print(Error)
-    finally:
-        con.close()
+    @property
+    def geocode(self):
+        for i in self.words:
+            response = requests.get(
+            "https://api.mapbox.com/geocoding/v5/mapbox.places/{}.json?access_token={}".format(i, self.mappass))
+            data = response.json()
+            self.long.append(data['features'][0]['center'][0])
+            self.lat.append(data['features'][0]['center'][1])
+
+    @property
+    def package_table(self):
+        data_tuples = list(zip(self.ids, self.words, self.lat, self.long))
+        return data_tuples
